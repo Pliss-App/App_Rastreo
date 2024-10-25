f3b4861b45112fb0b3e10e48b43c92d2fc01eddc
institutosanluis <insitutosanluis@gmail.com>
2024-10-25 10:55:07 -0600

    App Rastreo v1.5

--- controller/routesController.js
+++ controller/routesController.js
@@ -75,20 +75,22 @@ isRouter.get('/allDevices', authMiddleware.protect, async (req, res) => {
 
 isRouter.get('/idRoute', authMiddleware.protect, async (req, res) => {
     try {
-        const _id = req.query.id; 
-        const results = await isController.getRouteId( _id);
+        const _id = req.query.id;
+        const results = await isController.getRouteId(_id);
         if (results === undefined) {
             res.json({
                 error: 'Error, No existen registros!'
             })
         } else {
+            const data =  await isController.getRoutePoint(results.id);
+
             return res.status(200).json({
-              results: results,
+                results: {route: results, points: data}
             });
         }
 
     } catch (error) {
-        console.error('Error al obtener los roles:', error); 
+        console.error('Error al obtener los roles:', error);
 
         switch (error.code) {
             case 'ER_NO_SUCH_TABLE':
@@ -138,11 +140,11 @@ isRouter.get('/idRoute', authMiddleware.protect, async (req, res) => {
 
 isRouter.get('/idUser', authMiddleware.protect, async (req, res) => {
     try {
-        const _id = req.query.id; 
-        const _page = parseInt(req.query.page) || 1; 
-        const _limit = parseInt(req.query.limit) || 10; 
-        const _offset = (_page - 1) * _limit; 
-        
+        const _id = req.query.id;
+        const _page = parseInt(req.query.page) || 1;
+        const _limit = parseInt(req.query.limit) || 10;
+        const _offset = (_page - 1) * _limit;
+
 
         const results = await isController.getRouteRolIdUser(_limit, _offset, _id);
         if (results === undefined) {
@@ -151,13 +153,13 @@ isRouter.get('/idUser', authMiddleware.protect, async (req, res) => {
             })
         } else {
 
-            const totalUsers = results .total;
+            const totalUsers = results.total;
             const totalPages = Math.ceil(totalUsers / _limit);
             return res.status(200).json({
-              users: results,
-              currentPage: _page,
-              totalPages: totalPages,
-              totalUsers: totalUsers
+                users: results,
+                currentPage: _page,
+                totalPages: totalPages,
+                totalUsers: totalUsers
             });
 
             /*  return res.status(200).json({
@@ -217,11 +219,11 @@ isRouter.get('/idUser', authMiddleware.protect, async (req, res) => {
 
 isRouter.get('/all-routes', authMiddleware.protect, async (req, res) => {
     try {
-        const _id = req.query.id; 
-        const _page = parseInt(req.query.page) || 1; 
-        const _limit = parseInt(req.query.limit) || 10; 
-        const _offset = (_page - 1) * _limit; 
-        
+        const _id = req.query.id;
+        const _page = parseInt(req.query.page) || 1;
+        const _limit = parseInt(req.query.limit) || 10;
+        const _offset = (_page - 1) * _limit;
+
         const results = await isController.getRouteAll(_limit, _offset);
         if (results === undefined) {
             res.json({
@@ -229,13 +231,13 @@ isRouter.get('/all-routes', authMiddleware.protect, async (req, res) => {
             })
         } else {
 
-            const totalUsers = results .total;
+            const totalUsers = results.total;
             const totalPages = Math.ceil(totalUsers / _limit);
             return res.status(200).json({
-              users: results,
-              currentPage: _page,
-              totalPages: totalPages,
-              totalUsers: totalUsers
+                users: results,
+                currentPage: _page,
+                totalPages: totalPages,
+                totalUsers: totalUsers
             });
 
             /*  return res.status(200).json({
@@ -295,16 +297,28 @@ isRouter.get('/all-routes', authMiddleware.protect, async (req, res) => {
 
 isRouter.post('/add-route', async (req, res) => {
     try {
+        const routes = req.body.routes;
         const results = await isController.createRoute(req.body);
-        if (results === undefined) {
-            return res.status(200).json(result);
-
-        } else {
-
-            return res.status(200).json({
-                msg: 'Success',
-            });
+        if (!results || results.insertId === undefined) {
+            return res.status(500).json({ message: "Error al crear la ruta principal" });
         }
+
+        if (routes && routes.length > 0) {
+            for (const route of routes) {
+                const resulPoint = await isController.createRoutePoint(results.insertId, route);
+
+                // Si createRoutePoint falla, responde y detiene el proceso
+                if (!resulPoint) {
+                    return res.status(500).json({ message: "Error en createRoutePoint", data: resulPoint });
+                }
+            }
+        }
+
+
+        // Si todo es exitoso, responde con el ID de la ruta creada
+        return res.status(201).json({ message: "Ruta creada exitosamente", routeId: results.insertId });
+
+
     } catch (error) {
 
     }
@@ -312,59 +326,107 @@ isRouter.post('/add-route', async (req, res) => {
 
 isRouter.put('/update-status-route', authMiddleware.protect, async (req, res) => {
     try {
-      const {status, idUser, idRoute,  } = req.body;
-      const results = await isController.updateStatusRoute(status, idUser, idRoute);
-      if (results === undefined) {
-        res.json({
-          error: 'Error, No existe registro!'
-        })
-      } else {
-        return res.status(200).json({
-          msg: 'Successful Update',
-        });
-      }
-  
+        const { status, idUser, idRoute, } = req.body;
+        const results = await isController.updateStatusRoute(status, idUser, idRoute);
+        if (results === undefined) {
+            res.json({
+                error: 'Error, No existe registro!'
+            })
+        } else {
+            return res.status(200).json({
+                msg: 'Successful Update',
+            });
+        }
+
     } catch (error) {
-      console.error('Error al actualizar Estado de la Ruta:', error);  // Verificamos el código de error
+        console.error('Error al actualizar Estado de la Ruta:', error);  // Verificamos el código de error
+
+        // Manejo de errores según el código
+        switch (error.code) {
+            case 'ER_DUP_ENTRY':
+                // Error de entrada duplicada (ej. DPI o email ya existen en la base de datos)
+                console.error('DPI o correo electrónico ya existe.');
+                return res.status(400).json({
+                    error: error.sqlMessage
+                });
+
+            case 'ER_BAD_FIELD_ERROR':
+                // Error de campo incorrecto (cuando un campo de la consulta no existe en la base de datos)
+                console.error('Campo no válido en la consulta.');
+                return res.status(400).json({
+                    error: 'Error en la solicitud: el campo proporcionado no es válido.'
+                });
+
+            case 'ER_NO_REFERENCED_ROW':
+            case 'ER_ROW_IS_REFERENCED':
+                // Error de violación de llave foránea (cuando estás eliminando o insertando un valor que tiene dependencias)
+                console.error('Violación de llave foránea.');
+                return res.status(409).json({
+                    error: 'No puedes realizar esta acción porque hay registros relacionados en otra tabla.'
+                });
+
+            case 'ER_DATA_TOO_LONG':
+                // Error de longitud de dato (cuando intentas insertar un valor que excede la longitud permitida)
+                console.error('Dato demasiado largo para uno de los campos.');
+                return res.status(400).json({
+                    error: 'Uno de los campos supera la longitud permitida.'
+                });
+
+            default:
+                // Cualquier otro error no manejado específicamente
+                console.error('Error inesperado:', error);
+                return res.status(500).json({
+                    error: 'Ocurrió un error inesperado al actualizar la cuenta.'
+                });
+        }
+    }
+})
+
+
+isRouter.delete('/delete-route/:id', async (req, res) => {
+    try {
+        const results = await isController.deleteRouteId(req.params.id);
+        if (results === undefined) {
+          res.json({
+            error: 'Error, No existe cuenta'
+          })
+        } else {
+          return res.status(200).json({
+            result: '¡Delete Success!'
+          });
+        }
+      
+    } catch (err) {
+      console.error('Error al eliminar:', err); 
   
-      // Manejo de errores según el código
-      switch (error.code) {
+     switch (error.code) {
         case 'ER_DUP_ENTRY':
-          // Error de entrada duplicada (ej. DPI o email ya existen en la base de datos)
-          console.error('DPI o correo electrónico ya existe.');
           return res.status(400).json({
             error: error.sqlMessage
           });
   
         case 'ER_BAD_FIELD_ERROR':
-          // Error de campo incorrecto (cuando un campo de la consulta no existe en la base de datos)
-          console.error('Campo no válido en la consulta.');
           return res.status(400).json({
             error: 'Error en la solicitud: el campo proporcionado no es válido.'
           });
   
         case 'ER_NO_REFERENCED_ROW':
         case 'ER_ROW_IS_REFERENCED':
-          // Error de violación de llave foránea (cuando estás eliminando o insertando un valor que tiene dependencias)
-          console.error('Violación de llave foránea.');
           return res.status(409).json({
             error: 'No puedes realizar esta acción porque hay registros relacionados en otra tabla.'
           });
   
         case 'ER_DATA_TOO_LONG':
-          // Error de longitud de dato (cuando intentas insertar un valor que excede la longitud permitida)
-          console.error('Dato demasiado largo para uno de los campos.');
           return res.status(400).json({
             error: 'Uno de los campos supera la longitud permitida.'
           });
   
         default:
-          // Cualquier otro error no manejado específicamente
-          console.error('Error inesperado:', error);
           return res.status(500).json({
             error: 'Ocurrió un error inesperado al actualizar la cuenta.'
           });
       }
+  
     }
   })
 
--- models/routesModel.js
+++ models/routesModel.js
@@ -1,6 +1,6 @@
 const connection = require('../config/conexion');
 
-const createRoute = (data) => { //getByEmail
+const createRoute = (data) => {
     return new Promise((resolve, reject) => {
         connection.query(
             `INSERT INTO routes (idUser, 
@@ -23,6 +23,30 @@ const createRoute = (data) => { //getByEmail
             data.total_distance,
             data.total_time,
                 'Asignado'], (err, rows) => {
+                    if (err) {
+                        console.error('Error getting record:', err); // Registro del error en el servidor
+                        return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
+                    }
+                    resolve(rows);
+                });
+    });
+};
+
+const createRoutePoint = (idroute, data) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `INSERT INTO route_point (idroute,
+     direction,
+    lat,  
+    lng,  
+    distance, 
+    time) VALUES (?,?,?,?,?,?)`,
+            [idroute,
+                data.direction,
+                data.lat,
+                data.lng,
+                data.distance,
+                data.time], (err, rows) => {
                     if (err) {
                         console.error('Error getting record:', err); // Registro del error en el servidor
                         return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
@@ -104,7 +128,15 @@ const getRouteId = (_id) => { //getByEmail
 const getDevicesAll = (_id) => { //getByEmail
     return new Promise((resolve, reject) => {
         connection.query(
-            "SELECT * FROM locations WHERE 1", (err, rows) => {
+            `SELECT lo.*
+                from locations lo
+                inner join users us
+                ON lo.idUser =  us.id
+                INNER JOIN user_permission up
+                ON us.id = up.iduser
+                INNER JOIN permission p 
+                ON up.permission_id = p.id WHERE p.nombre_permiso='cliente'`,
+            (err, rows) => {
                 if (err) {
                     console.error('Error getting record:', err); // Registro del error en el servidor
                     return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
@@ -118,7 +150,33 @@ const getRouteRolIdUser = (_limit, _offset, _id) => {
 
     return new Promise((resolve, reject) => {
         connection.query(
-            "SELECT * FROM routes WHERE idUser = ? LIMIT ? OFFSET ?", [_id, _limit, _offset], (err, rows) => {
+            `SELECT us.name, us.last_name, r.*,  COALESCE(GROUP_CONCAT(rp.direction ORDER BY rp.id SEPARATOR ', '), NULL) AS additional_directions FROM routes r
+            INNER JOIN users us
+            ON r.idUser = us.id
+           LEFT JOIN   route_point rp
+            ON rp.idroute = r.id
+            where r.idUser= ?
+            GROUP BY r.id  LIMIT ? OFFSET ?`, [_id, _limit, _offset], (err, rows) => {
+            if (err) {
+                console.error('Error getting record:', err); // Registro del error en el servidor
+                return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
+            }
+            resolve(rows);
+        });
+    });
+};
+
+const getRouteAll = (_limit, _offset,) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT us.name, us.last_name, r.*,  COALESCE(GROUP_CONCAT(rp.direction ORDER BY rp.id SEPARATOR ', '), NULL) AS additional_directions FROM routes r
+            INNER JOIN users us
+            ON r.idUser = us.id
+           LEFT JOIN   route_point rp
+            ON rp.idroute = r.id
+            GROUP BY 
+    r.id LIMIT ? OFFSET ?`,
+            [_limit, _offset], (err, rows) => {
                 if (err) {
                     console.error('Error getting record:', err); // Registro del error en el servidor
                     return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
@@ -128,19 +186,31 @@ const getRouteRolIdUser = (_limit, _offset, _id) => {
     });
 };
 
-const getRouteAll = (_limit, _offset,) => {
+const getRoutePoint = (id) => {
     return new Promise((resolve, reject) => {
         connection.query(
-            `SELECT us.name, us.last_name, r.* FROM routes r
-            INNER JOIN users us
-            ON r.idUser = us.id LIMIT ? OFFSET ?`, 
-        [_limit, _offset], (err, rows) => {
-            if (err) {
-                console.error('Error getting record:', err); // Registro del error en el servidor
-                return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
-            }
-            resolve(rows);
-        });
+            `SELECT * FROM route_point where idroute = ?`,
+            [id], (err, rows) => {
+                if (err) {
+                    console.error('Error getting record:', err); // Registro del error en el servidor
+                    return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
+                }
+                resolve(rows);
+            });
+    });
+};
+
+const deleteRouteId = (id) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `DELETE FROM routes WHERE id = ${id};`,
+            (err, rows) => {
+                if (err) {
+                    console.error('Error getting record:', err); // Registro del error en el servidor
+                    return reject(new Error('Error getting record')); // Rechazo con un mensaje de error personalizado
+                }
+                resolve(rows);
+            });
     });
 };
 
@@ -153,5 +223,8 @@ module.exports = {
     getRouteRolIdUser,
     getRouteAll,
     getRouteId,
-    updateStatusRoute
+    updateStatusRoute,
+    createRoutePoint,
+    deleteRouteId,
+    getRoutePoint
 }
